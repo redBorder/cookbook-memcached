b13328bab9b6160fea579596f935ffafe49e28f6
Miguel Negron <manegron@redborder.com>
2023-09-26 22:45:57 +0100

    Match metadata version with tag

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'ejimenez@redborder.com'
 license          'All rights reserved'
 description      'Installs/Configures memcached'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.2'
+version          '1.0.2'
