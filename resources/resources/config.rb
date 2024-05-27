080b32d314b0d6e294bbabc9453899b0337a6a8e
Miguel Negron <manegron@redborder.com>
2024-05-27 19:42:56 +0100

    Lint resources

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -1,16 +1,14 @@
-# Cookbook Name:: memcached
-#
+# Cookbook:: memcached
 # Resource:: config
-#
 
 actions :add, :remove, :register, :deregister
 default_action :add
 
-attribute :memory, :kind_of => Fixnum, :default => 524288
-attribute :user, :kind_of => String, :default => "memcached"
-attribute :group, :kind_of => String, :default => "memcached"
-attribute :port, :kind_of => Fixnum, :default => 11211
-attribute :maxconn, :kind_of => Fixnum, :default => 1024
-attribute :cachesize, :kind_of => Fixnum, :default => 512
-attribute :options, :kind_of => String, :default => "-I 10m -v"
-attribute :ipaddress, :kind_of => String, :default => "127.0.0.1"
+attribute :memory, kind_of: Integer, default: 524288
+attribute :user, kind_of: String, default: 'memcached'
+attribute :group, kind_of: String, default: 'memcached'
+attribute :port, kind_of: Integer, default: 11211
+attribute :maxconn, kind_of: Integer, default: 1024
+attribute :cachesize, kind_of: Integer, default: 512
+attribute :options, kind_of: String, default: '-I 10m -v'
+attribute :ipaddress, kind_of: String, default: '127.0.0.1'
